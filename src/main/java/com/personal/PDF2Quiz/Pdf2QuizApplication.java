f5c44865d8786313def3bf627317a2bf6546c461
Ayush9852 <k.ayush9835@gmail.com>
2025-07-08 17:58:43 +0530

    Initial commit for PDF2Quiz project

---
+++ src/main/java/com/personal/PDF2Quiz/Pdf2QuizApplication.java
@@ -0,0 +1,13 @@
+package com.personal.PDF2Quiz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Pdf2QuizApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Pdf2QuizApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/personal/PDF2Quiz/Pdf2QuizApplicationTests.java
@@ -0,0 +1,13 @@
+package com.personal.PDF2Quiz;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Pdf2QuizApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
